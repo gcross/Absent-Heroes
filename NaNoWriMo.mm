d8244c08d7e7def969ff12deffcfaf39609ba019
Gregory Crosswhite <gcross@phys.washington.edu>
2008-10-26 13:56:21 -0700

    Created project and initial mindmap.

---
+++ NaNoWriMo.mm
@@ -0,0 +1,109 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1224986822981" ID="Freemind_Link_418268625" MODIFIED="1224986830991" TEXT="NaNoWriMo">
+<node CREATED="1224986832608" ID="_" MODIFIED="1224986837015" POSITION="right" TEXT="Character #1">
+<node CREATED="1224986843571" ID="Freemind_Link_566252059" MODIFIED="1224986852046" TEXT="wants to be a hero"/>
+<node CREATED="1224986852411" ID="Freemind_Link_659717688" MODIFIED="1224986866038" TEXT="problems">
+<node CREATED="1224986854435" ID="Freemind_Link_233835833" MODIFIED="1224986860566" TEXT="absent minded"/>
+<node CREATED="1224986861116" ID="Freemind_Link_96602282" MODIFIED="1224986873933" TEXT="guys always fall for the guy behind her">
+<node CREATED="1224987619614" ID="Freemind_Link_612888354" MODIFIED="1224987645089" TEXT="dammit, why does that always happen to me?  at least it was a girl that time, believe me, nothing makes you feel less feminine"/>
+</node>
+</node>
+<node CREATED="1224986878522" ID="Freemind_Link_1081226086" MODIFIED="1224986888125" TEXT="main character?"/>
+<node CREATED="1224986889041" ID="Freemind_Link_1994096339" MODIFIED="1224986895021" TEXT="decides to become a villain?">
+<node CREATED="1224987575632" ID="Freemind_Link_1040327771" MODIFIED="1224987580011" TEXT="other villains don&apos;t appreciate the mockery"/>
+<node CREATED="1224987710594" ID="Freemind_Link_1643072919" MODIFIED="1224987715430" TEXT="serious, or deliberately bad?"/>
+<node CREATED="1224987715938" ID="Freemind_Link_232388355" MODIFIED="1224987720990" TEXT="first business model is to be serious"/>
+<node CREATED="1224987722162" ID="Freemind_Link_977895644" MODIFIED="1224987733149" TEXT="later business model is to have non-heros pay for the experience"/>
+</node>
+<node CREATED="1224987824302" ID="Freemind_Link_1944367414" MODIFIED="1224987826914" TEXT="needs a sidekick?"/>
+<node CREATED="1224987829534" ID="Freemind_Link_224351599" MODIFIED="1224987834049" TEXT="PLOT:  quest for purpose in live"/>
+<node CREATED="1224987834438" ID="Freemind_Link_226095010" MODIFIED="1224987842801" TEXT="can kick major ass, but just always leaves out key detail"/>
+<node CREATED="1224989735038" ID="Freemind_Link_1456153334" MODIFIED="1224989737737" TEXT="personality">
+<node CREATED="1224989738046" ID="Freemind_Link_1711422543" MODIFIED="1224989742529" TEXT="driven"/>
+<node CREATED="1224989743022" ID="Freemind_Link_132352258" MODIFIED="1224989745337" TEXT="resourceful"/>
+<node CREATED="1224989745558" ID="Freemind_Link_1774917489" MODIFIED="1224990634976" TEXT="short temper, but also easily amused"/>
+<node CREATED="1224990636796" ID="Freemind_Link_971031536" MODIFIED="1224990637896" TEXT="firey?"/>
+<node CREATED="1224989750789" ID="Freemind_Link_1380903824" MODIFIED="1224989754905" TEXT="forgetful at key moments">
+<node CREATED="1224989764341" ID="Freemind_Link_815114269" MODIFIED="1224989785656" TEXT="revelations -- need to try to keep the peasants out, rather than trying to make it be too easy"/>
+</node>
+<node CREATED="1224990641620" ID="Freemind_Link_1424925908" MODIFIED="1224990647296" TEXT="loose morals -- no problem with switching sides"/>
+<node CREATED="1224990647644" ID="Freemind_Link_1337827869" MODIFIED="1224990662393" TEXT="is in this because of her passion for the fight rather than a passion to save, oppress, or profit per se"/>
+<node CREATED="1224994212925" ID="Freemind_Link_649116905" MODIFIED="1224994217104" TEXT="very sensitive"/>
+</node>
+<node CREATED="1224992000353" ID="Freemind_Link_1072838671" MODIFIED="1224992001124" TEXT="traits">
+<node CREATED="1224992001392" ID="Freemind_Link_500297653" MODIFIED="1224992019316" TEXT="except for her absent-mindedness, she&apos;s actually the toughest of all the heros, though she has a soft spot for romance">
+<node CREATED="1224992030095" ID="Freemind_Link_1738133348" MODIFIED="1224992055018" TEXT="&quot;I&apos;m actually a softie inside&quot; -- kicks guy who is looking at her -- &quot;I only fall for the cute ones&quot;"/>
+<node CREATED="1224992061270" ID="Freemind_Link_1712802293" MODIFIED="1224992074131" TEXT="she can beat people up better than any other hero, which is why they keep her on for so long"/>
+</node>
+<node CREATED="1224992541316" ID="Freemind_Link_1956804943" MODIFIED="1224992561039" TEXT="considered too dangerous to be on a team">
+<node CREATED="1224992561331" ID="Freemind_Link_529148157" MODIFIED="1224992568432" TEXT="doesn&apos;t slow down for her compatriarts"/>
+<node CREATED="1224992568771" ID="Freemind_Link_1475145618" MODIFIED="1224992578416" TEXT="sometimes forgets they are there and hurts them"/>
+<node CREATED="1224992581059" ID="Freemind_Link_23181261" MODIFIED="1224992595119" TEXT="forgets key details that screw up everyone"/>
+<node CREATED="1224992596626" ID="Freemind_Link_668925075" MODIFIED="1224992624533" TEXT="frankly, the only reason she is kept on a hero salary is because she is such an effective fighter by herself"/>
+</node>
+</node>
+</node>
+<node CREATED="1224989442514" ID="Freemind_Link_371548378" MODIFIED="1224989448703" POSITION="right" TEXT="Character #2">
+<node CREATED="1224989459241" ID="Freemind_Link_1448688564" MODIFIED="1224989489195" TEXT="gay friend and companion of character #1"/>
+</node>
+<node CREATED="1224987755289" ID="Freemind_Link_722846233" MODIFIED="1224989458124" POSITION="right" TEXT="Character #?">
+<node CREATED="1224987758184" ID="Freemind_Link_1781311799" MODIFIED="1224987771180" TEXT="evil, goes on rant about how a true bad character must have evil in their blood"/>
+</node>
+<node CREATED="1224987347001" ID="Freemind_Link_347902978" MODIFIED="1224987348185" POSITION="right" TEXT="Dare: Have a character (not necessarily the main character) complain that &quot;this scene wouldn&apos;t be in a good book&quot;)"/>
+<node CREATED="1224986901634" ID="Freemind_Link_668171483" MODIFIED="1224986903149" POSITION="left" TEXT="Scenes">
+<node CREATED="1224986904257" ID="Freemind_Link_295145208" MODIFIED="1224986908260" TEXT="Start of book">
+<node CREATED="1224986909177" ID="Freemind_Link_1706404280" MODIFIED="1224986916932" TEXT="hero is doing something, and forgets crucial detail"/>
+</node>
+<node CREATED="1224989470808" ID="Freemind_Link_1235047405" MODIFIED="1224989472372" TEXT="Rejection">
+<node CREATED="1224989473424" ID="Freemind_Link_1757077337" MODIFIED="1224989477444" TEXT="first for girl behind her"/>
+<node CREATED="1224989477728" ID="Freemind_Link_1885382590" MODIFIED="1224989481947" TEXT="later for gay friend"/>
+</node>
+</node>
+<node CREATED="1224989499167" ID="Freemind_Link_901248434" MODIFIED="1224989502043" POSITION="left" TEXT="Possible plot outline">
+<node CREATED="1224989502567" ID="Freemind_Link_421571801" MODIFIED="1224989510994" TEXT="H starts as hero"/>
+<node CREATED="1224989511311" ID="Freemind_Link_697391517" MODIFIED="1224989515570" TEXT="gives up, tries being enemy"/>
+<node CREATED="1224989516094" ID="Freemind_Link_1332953024" MODIFIED="1224989527313" TEXT="real heros refuse to fight her any more because she is not really trying"/>
+<node CREATED="1224989527638" ID="Freemind_Link_1542359157" MODIFIED="1224989555064" TEXT="eventually realizes she can sell adventures to peasants"/>
+<node CREATED="1224989555469" ID="Freemind_Link_150224081" MODIFIED="1224989566728" TEXT="bad guys mock her at first, but eventually all switch to her business model"/>
+<node CREATED="1224989567108" ID="Freemind_Link_860507419" MODIFIED="1224989577032" TEXT="good guys get sick of having no enemies, and become bad guys themselves"/>
+<node CREATED="1224989577484" ID="Freemind_Link_47354140" MODIFIED="1224989588847" TEXT="bad guys stop good guys to save their business model"/>
+<node CREATED="1224989589820" ID="Freemind_Link_1013534961" MODIFIED="1224989599711" TEXT="good guys are given MMORPGs to keep them occupied"/>
+</node>
+<node CREATED="1224990188269" ID="Freemind_Link_1848534174" MODIFIED="1224990190121" POSITION="left" TEXT="Hero tax">
+<node CREATED="1224990171381" ID="Freemind_Link_1216252486" MODIFIED="1224990184993" TEXT="Money is raised from peasants by &quot;Hero Tax&quot; to defend them from the bad guys"/>
+<node CREATED="1224990196013" ID="Freemind_Link_810888623" MODIFIED="1224990228607" TEXT="When all of the bad guys stop terrorizing the peasants (since they really only ever broke even this way with all of the heros raiding), the heros are no longer able to collect their taxes and rebel and turn into bad guy"/>
+<node CREATED="1224990229067" ID="Freemind_Link_882123925" MODIFIED="1224990246166" TEXT="now the former bad guys actually have to fight the heros to save the peasants to protect their income base"/>
+<node CREATED="1224990442612" ID="Freemind_Link_71937703" MODIFIED="1224990455168" TEXT="maybe it&apos;s talking to the peasants that gives her the idea in the first place?"/>
+</node>
+<node CREATED="1224990817637" ID="Freemind_Link_998859199" MODIFIED="1224990822769" POSITION="left" TEXT="Heros turned bad guys">
+<node CREATED="1224990823109" ID="Freemind_Link_9456793" MODIFIED="1224990833209" TEXT="they&apos;re actually much worse, or much better at it than the actual bad guys?"/>
+<node CREATED="1224990838460" ID="Freemind_Link_21541663" MODIFIED="1224990857327" TEXT="they&apos;re actually really good at it since they don&apos;t suffer from the same absent-mindedness, arrogance, etc as he bad guys"/>
+<node CREATED="1224990857548" ID="Freemind_Link_1845684351" MODIFIED="1224990863464" TEXT="team of bad guys form motley team to take them on"/>
+</node>
+<node CREATED="1224990940841" ID="Freemind_Link_1832476605" MODIFIED="1224990989538" POSITION="left" TEXT="Peasants are hesitant to accept the reformed bad guys at first">
+<node CREATED="1224990954320" ID="Freemind_Link_1869421376" MODIFIED="1224990956331" TEXT="appeasement?"/>
+<node CREATED="1224990959160" ID="Freemind_Link_303069863" MODIFIED="1224990967523" TEXT="youngins who don&apos;t remember are more likely to accept"/>
+<node CREATED="1224990969775" ID="Freemind_Link_447322765" MODIFIED="1224990978267" TEXT="some say this is the best way to get them for the good, others protest"/>
+<node CREATED="1224990996430" ID="Freemind_Link_843302917" MODIFIED="1224991006003" TEXT="after all, they had done awefully terrible things in times of yore"/>
+</node>
+<node CREATED="1224991059108" ID="Freemind_Link_1993271148" MODIFIED="1224991062824" POSITION="right" TEXT="More plot points">
+<node CREATED="1224991508443" ID="Freemind_Link_1436670248" MODIFIED="1224991517776" TEXT="she gets fired, no longer gets public financing"/>
+<node CREATED="1224991522770" ID="Freemind_Link_614562683" MODIFIED="1224991528878" TEXT="someone questionable arrives, offers to team up with her"/>
+<node CREATED="1224991063412" ID="Freemind_Link_1784425486" MODIFIED="1224991072312" TEXT="hero teams up with someone to go with her and make sure she remembers everything"/>
+<node CREATED="1224991073787" ID="Freemind_Link_1859164778" MODIFIED="1224991088199" TEXT="at the last minute he reveals himself to be a villain, and steals the loot"/>
+<node CREATED="1224991088603" ID="Freemind_Link_1665410658" MODIFIED="1224991091742" TEXT="she swears revenge"/>
+<node CREATED="1224991091995" ID="Freemind_Link_1951275640" MODIFIED="1224991100471" TEXT="for once, she actually makes it past all of his defences"/>
+<node CREATED="1224991101098" ID="Freemind_Link_1519473669" MODIFIED="1224991125750" TEXT="at the end, she realizes that bad guys are actually just as absent-minded as she is -- she could fit in with this group"/>
+<node CREATED="1224991125985" ID="Freemind_Link_1257581727" MODIFIED="1224991144504" TEXT="does she kill him, violating the code?"/>
+<node CREATED="1224991144865" ID="Freemind_Link_948070392" MODIFIED="1224991152741" TEXT="she steals all of his treasure, violating the code?"/>
+<node CREATED="1224991153912" ID="Freemind_Link_1556600772" MODIFIED="1224991165653" TEXT="anyway, after she&apos;s done with him, there is no more need for revenge since it was just taken"/>
+<node CREATED="1224991197775" ID="Freemind_Link_1473746278" MODIFIED="1224991207362" TEXT="she&apos;s not actually evil, though, so she does silly things to get people to pay her?">
+<node CREATED="1224991207630" ID="Freemind_Link_271892743" MODIFIED="1224991212979" TEXT="or at least, not terribly aweful thing"/>
+<node CREATED="1224991213374" ID="Freemind_Link_46785548" MODIFIED="1224991216610" TEXT="maybe she just bluffs?"/>
+</node>
+</node>
+<node CREATED="1224991602272" ID="Freemind_Link_1440428906" MODIFIED="1224991611852" POSITION="right" TEXT="Motif -- fantasy or sci-fi?  maybe a blend of both"/>
+<node CREATED="1224994622174" ID="Freemind_Link_190280360" MODIFIED="1224994637929" POSITION="left" TEXT="story could end with her getting hit on by a woman -- &quot;ah well, I guess you can&apos;t have everything&quot;"/>
+</node>
+</map>
